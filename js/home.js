b2ba2a1ef817fcc8b87510e1e69106697b72cf22
wilson-lin <dannyway741006@gmail.com>
2021-02-03 19:52:05 +0800

    no message

--- js/home.js
+++ js/home.js
@@ -12,33 +12,33 @@ let newTypeData = [
   '工商服務'
 ];
 let typeData = [{
-    title: '旅遊美食',
-    cat: '旅遊美食'
-  },
-  {
-    title: '交通運輸',
-    cat: '交通運輸'
-  },
-  {
-    title: '商業學術',
-    cat: '商業學術'
-  },
-  {
-    title: '生活服務',
-    cat: '生活服務'
-  },
-  {
-    title: '居家裝潢',
-    cat: '居家裝潢'
-  },
-  {
-    title: '醫療保健',
-    cat: '醫療保健'
-  },
-  {
-    title: '工商服務',
-    cat: '工商服務'
-  },
+  title: '旅遊美食',
+  cat: '旅遊美食'
+},
+{
+  title: '交通運輸',
+  cat: '交通運輸'
+},
+{
+  title: '商業學術',
+  cat: '商業學術'
+},
+{
+  title: '生活服務',
+  cat: '生活服務'
+},
+{
+  title: '居家裝潢',
+  cat: '居家裝潢'
+},
+{
+  title: '醫療保健',
+  cat: '醫療保健'
+},
+{
+  title: '工商服務',
+  cat: '工商服務'
+},
 ];
 let cityData = ['基隆市', '臺北市', '新北市', "桃園市", "新竹市", "新竹縣", "苗栗縣", "臺中市", "彰化縣",
   "南投縣", "雲林縣", "嘉義市", "嘉義縣", "臺南市", "高雄市", "屏東縣", "臺東縣", "花蓮縣", "宜蘭縣",
@@ -439,13 +439,13 @@ new Vue({
 
     fireData() {
       db.collection('shop1688web')
-        // .limit(12)
+        // .limit(246)
         .get()
         .then(querySnapshot => {
-          // let i = 1;
+          let i = 1;
           querySnapshot.forEach(doc => {
             // console.log(i++)
-            // console.log(i++, doc.data().bid);
+            console.log(i++, doc.data().bid);
             let temp = {
               title: '',
               cat: ''
@@ -958,11 +958,11 @@ new Vue({
     geocodeResults(geocoder, resultsMap, address) {
       return new Promise((resolve, reject) => {
         geocoder.geocode({
-            address: address,
-            componentRestrictions: {
-              country: "TW",
-            },
+          address: address,
+          componentRestrictions: {
+            country: "TW",
           },
+        },
 
           (results, status) => {
             if (status === "OK") {
